79379317b95bdffe7031e853e9c05595fa2ef9fe
faregosoftware@gmail.com <faregosoftware@gmail.com>
2021-04-08 22:03:01 -0400

    Agregando API

---
+++ Faregosoft.Api/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Faregosoft.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Faregosoft.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        }
+    }
+}
