9d577d13afdd37f6f15b44e7821d614b39c79f5d
faregosoftware@gmail.com <faregosoftware@gmail.com>
2021-04-09 23:11:36 -0400

    Login UI

--- FaregosoftJR/FaregosoftJR.Shared/App.xaml.cs
+++ FaregosoftJR/FaregosoftJR.Shared/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FaregosoftJR.Pages;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -88,7 +89,7 @@ namespace FaregosoftJR
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(LoginPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 window.Activate();
---
+++ FaregosoftJR/FaregosoftJR.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Faregosoft.Helpers;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FaregosoftJR.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog messageDialog;
+            if (string.IsNullOrEmpty(EmailTbx.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            if (!RegexUtilities.IsValidEmail(EmailTbx.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email valido", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(PasswordPbx.Password))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            messageDialog = new MessageDialog("Vamos bien!", "OK");
+            await messageDialog.ShowAsync();
+
+        }
+    }
+}
